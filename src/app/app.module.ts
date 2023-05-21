40fd4c48e26b5293ceefce6c2791f8a080b46c0d
serratos <jserratos.ies@segurosatlas.com.mx>
2023-05-20 19:08:50 -0600

    Template con rutas configuradas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { IngresoEgresoComponent } from './ingreso-egreso/ingreso-egreso.component';
+import { EstadisticaComponent } from './ingreso-egreso/estadistica/estadistica.component';
+import { DetalleComponent } from './ingreso-egreso/detalle/detalle.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SidbarComponent } from './shared/sidbar/sidbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    IngresoEgresoComponent,
+    EstadisticaComponent,
+    DetalleComponent,
+    FooterComponent,
+    NavbarComponent,
+    SidbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/sidbar/sidbar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidbar',
+  templateUrl: './sidbar.component.html',
+  styles: [
+  ]
+})
+export class SidbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
